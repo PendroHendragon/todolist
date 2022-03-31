137c05eb0be678d5f2ecd7018b49ceae6080cfcb
Pedro Mattos <phmattos0@gmail.com>
2022-03-30 21:25:05 -0300

    changes

---
+++ assets/components/task.mjs
@@ -0,0 +1,12 @@
+
+
+
+
+
+function task(task){
+    const template = `<div class='task'><h4>${task} </h4><button class='btn'>Complete task</button></div>`;
+    return template;
+
+}
+
+export default task;
---
+++ index.mjs
@@ -0,0 +1,12 @@
+import task from './assets/components/task.mjs';
+
+
+
+let button = document.querySelector('#btn');
+
+button.addEventListener('click', function(){
+    let input = document.querySelector('#input');
+    let tasks = document.querySelector('#tasks');
+    tasks.innerHTML += task(input.value)
+    
+})
